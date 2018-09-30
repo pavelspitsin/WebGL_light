c7d23fb24855fe2441ff6205e70e7bae500c2cd0
Pavel <pavel.spitsin@yandex.ru>
2018-09-30 15:49:01 +0300

    Added crysis 2 8bit sound

--- js/switches.js
+++ js/switches.js
@@ -1,4 +1,66 @@
+'use strict';
+
+class CrysisAudio {
+    constructor() {
+        let url = './resources/Music/crysis2.ogg';
+        this._crysisAudio = new Audio(url);
+        this._crysisAudio.volume = 0.5;
+        this.btnElement = document.getElementById('musicBtn');
+    }
+
+    audioBtnClick() {
+
+        if (this._crysisAudio.paused) {
+            this.btnElement.innerHTML = 'Music (playing)';
+            this.audioPlay();
+        }
+        else {
+            this.btnElement.innerHTML = 'Music (pause)';
+            this.audioPause();
+        }
+    }
+
+    hideBtn() {
+        this.btnElement.style.visibility = 'hidden';
+    }
+    
+    showBtn() {
+        this.btnElement.style.visibility = 'visible';
+    }
+
+    
+    audioReset() {
+        document.getElementById('musicBtn').innerHTML = 'Music';
+        this._crysisAudio.pause();
+        this._crysisAudio.currentTime = 0; 
+    }
+
+    audioPause() {
+        this._crysisAudio.pause();
+    }
+
+    audioPlay() {
+        this._crysisAudio.play();
+    }
+}
+
+
+
+const crysisAudio = new CrysisAudio();
+
+
+
 function models_combobox_changed(objCombo) {    
+
+    if (objCombo.value !== 'nanosuit') {
+        crysisAudio.audioPause();
+        crysisAudio.audioReset();
+        crysisAudio.hideBtn();
+    }
+    else {        
+        crysisAudio.showBtn();
+    }
+
     setCurrentModel(objCombo.value);   
 }
 
@@ -20,4 +82,6 @@ function initSwitches() {
     document.getElementById('rotate_checkbox').checked = _state.isRotate;
     document.getElementById('diffuse_checkbox').checked = _state.isUseDiffuseMap;
     document.getElementById('normal_checkbox').checked = _state.isUseNormalMap;
+    
+    crysisAudio.hideBtn();
 }
